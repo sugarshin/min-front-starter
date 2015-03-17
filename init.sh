c5cfd218cefc683a76110ba787b09aab00fef528
sato <sato@tsumikiinc.com>
2015-03-17 18:33:45 +0900

    edit

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 npm i -D cssify crel beefy browserify coffeeify
-mkdir css && echo '@charset "utf-8";' > css/index.css
+mkdir css public && echo '@charset "utf-8";' > css/index.css
 rm -rf .git init.sh
